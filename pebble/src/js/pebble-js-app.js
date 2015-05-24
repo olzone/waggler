f4374fc85c62e2ad935bdef4e83cb69fa7d43481
laiqu <laiquendir@gmail.com>
2015-05-24 03:03:57 +0200

    Pebble app update, and test data.

--- pebble/src/js/pebble-js-app.js
+++ pebble/src/js/pebble-js-app.js
@@ -1,34 +1,76 @@
 Pebble.addEventListener("ready",
     function(e) {
         console.log("Hello world! - Sent from your javascript application.");
-/*
-        ws = new WebSocket('ws://192.168.43.107:8000');
-        ws.onopen = function(evt) {
-            console.log('asdf');
-            ws.send('sent app msg');
-        }
-
-        ws.onmessage(function (evt) {
-//            json = JSON.parse(evt.data);
-            console.log('received:' + evt.data);
-        });
-*/
-        var transactionId = Pebble.sendAppMessage( { '0': 42, '1': 'String value' },
-          function(e) {
-            console.log('Successfully delivered message with transactionId=' + e.data.transactionId);
-          },
-          function(e) {
-            console.log('Unable to deliver message with transactionId='
-              + e.data.transactionId
-              + ' Error is: ' + e.error.message);
-          }
-        );
-
+        ws = null;
+        sendMsg({'7':0});
+        msg_queue = [];
     }
 );
 
+function sendMsg(data) {
+    return Pebble.sendAppMessage( data,
+        function(e) {
+            console.log('Successfully delivered message with transactionId=' + e.data.transactionId);
+        },
+        function(e) {
+            console.log('Unable to deliver message with transactionId='
+              + e.data.transactionId
+              + ' Error is: ' + e.error.message);
+        }
+    );
+}
+
+function isFlushNeeded() {
+    for (var i = 0; i < msg_queue.length; i++) {
+        if (msg_queue[i]['dummy'] == 'END' ||
+                msg_queue[i]['dummy'] == 'START') {
+            return true;
+        }
+    }
+    return false;
+}
+
 Pebble.addEventListener('appmessage',
   function(e) {
-    console.log('Received message: ' + JSON.stringify(e.payload));
+//    console.log('Received message: ' + JSON.stringify(e.payload));
+    
+    if (ws === null || ws.readyState > WebSocket.OPEN) {
+        //echo to 107
+        //krol to 38
+        ws = new WebSocket('ws://192.168.43.107:8000');
+    }
+    
+    console.log(JSON.stringify(e.payload));
+    if (e.payload['dummy'] == 'ACC_DATA') {
+        var points = {}
+        for (var key in e.payload) {
+            if (key != 'dummy') {
+                var i_key = parseInt(key);
+                var index = Math.floor((i_key - 1) / 3);
+                if (!(index in points)) {
+                    points[index] = [];
+                }
+                points[index][(i_key - 1) % 3] = e.payload[key];
+            }
+        }
+        console.log(JSON.stringify(points));
+        for (var i in points) {
+            var obj = {
+                'X' : points[i][0],
+                'Y' : points[i][1],
+                'Z' : points[i][2],
+                'dummy' : 'ACC_DATA',
+            };
+            console.log(JSON.stringify(obj));
+            msg_queue.push(obj);
+        }
+    } else {
+        msg_queue.push(e.payload);
+    }
+    //console.log('msgs:' + JSON.stringify(msg_queue));
+    if ((msg_queue.length > 10 || isFlushNeeded()) && ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify(msg_queue));
+        msg_queue = [];
+    }
   }
 );
--- pebble/src/pebble.c
+++ pebble/src/pebble.c
@@ -1,16 +1,40 @@
 #include <pebble.h>
-#define ACC_SAMPLES 1
+#define ACC_SAMPLES 3
+#define ACC_BATCH_SIZE 5
+#define SAMPLES (ACC_SAMPLES * ACC_BATCH_SIZE)
+#define JS_READY 7
 
 static Window *window;
 static TextLayer *text_layer;
+static bool js_ready = false;
+static int recording = false;
+static bool ended = false;
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+    static const char* type = "SELECT";
+    if (js_ready) {
+        DictionaryIterator* iter;
+        app_message_outbox_begin(&iter); 
+        dict_write_cstring(iter, 0, type);
+        app_message_outbox_send();
+    }
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+    if (js_ready) {
+        ended = true;
+    }
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+    static const char* type = "START";
+    if (js_ready) {
+        DictionaryIterator* iter;
+        app_message_outbox_begin(&iter); 
+        dict_write_cstring(iter, 0, type);
+        app_message_outbox_send();
+        recording = true;
+    }
 }
 
 static void click_config_provider(void *context) {
@@ -33,15 +57,13 @@ static void window_unload(Window *window) {
     text_layer_destroy(text_layer);
 }
 
-#define JS_READY 7
-bool js_ready = false;
-
 static void inbox_received(DictionaryIterator* iterator, void* context) {
     APP_LOG(APP_LOG_LEVEL_INFO, "inbox received");
     Tuple *tuple = dict_read_first(iterator);
     while (tuple) {
       switch (tuple->key) {
         case JS_READY:
+          js_ready = true;
           break;
         case 1:
           APP_LOG(APP_LOG_LEVEL_INFO, tuple->value->cstring);
@@ -66,23 +88,50 @@ static void outbox_sent(DictionaryIterator* iterator, void* context) {
     APP_LOG(APP_LOG_LEVEL_INFO, "outbox sent");
 }
 
+static void flush_acc_batch(AccelData* batch, uint32_t dict_size) {
+    static const char* type = "ACC_DATA";
+    DictionaryIterator* iter;
+    app_message_outbox_begin(&iter); 
+    dict_write_cstring(iter, 0, type);
+    for (uint32_t i = 0; i < dict_size; i++) {
+        dict_write_int(iter, i * 3 + 1, &batch[i].x, sizeof(short int), true);
+        dict_write_int(iter, i * 3 + 2, &batch[i].y, sizeof(short int), true);
+        dict_write_int(iter, i * 3 + 3, &batch[i].z, sizeof(short int), true);
+    }
+    app_message_outbox_send();
+}
+
 static void acc_handler(AccelData* data, uint32_t samples) {
-    const char* type = "ACC_DATA";
     static char d_buff[128];
-    snprintf(d_buff, sizeof(d_buff), "%d %d %d", data[0].x, data[0].y, data[0].z);
+    static AccelData batch[SAMPLES];
+    static const char* end = "END";
+    static uint32_t dict_size = 0;
+    static int reads = 0;
+    reads++;
+    snprintf(d_buff, sizeof(d_buff), "[%d %d %d] %d", data[0].x, data[0].y, data[0].z, reads);
     text_layer_set_text(text_layer, d_buff);
     
-    for (uint32_t i = 0; i < samples; i++) {
-        APP_LOG(APP_LOG_LEVEL_INFO, d_buff);
-    /*    DictionaryIterator* iter;
-        app_message_outbox_begin(&iter); 
-        dict_write_cstring(iter, 0, type);
-        dict_write_int(iter, 1, &data[i].x, sizeof(int), true);
-        dict_write_int(iter, 2, &data[i].y, sizeof(int), true);
-        dict_write_int(iter, 3, &data[i].z, sizeof(int), true);
-        app_message_outbox_send();*/
+    if (js_ready && recording) {
+        for (uint32_t i = 0; i < samples; i++) {
+            APP_LOG(APP_LOG_LEVEL_INFO, d_buff);
+            batch[dict_size++] = data[i];
+        }
     }
-    
+    if (dict_size >= SAMPLES ||
+            ended == true) {
+        if (dict_size > 0) {
+            flush_acc_batch(batch, dict_size);
+        }
+        if (ended) {
+            DictionaryIterator* iter;
+            app_message_outbox_begin(&iter); 
+            dict_write_cstring(iter, 0, end);
+            app_message_outbox_send();
+            recording = false;
+            ended = false;
+        }
+        dict_size = 0;
+    }
 }
 
 static void init(void) {
@@ -95,7 +144,7 @@ static void init(void) {
 
     // acc
     accel_data_service_subscribe(ACC_SAMPLES, acc_handler);
-    accel_service_set_sampling_rate(ACCEL_SAMPLING_10HZ);
+    accel_service_set_sampling_rate(ACCEL_SAMPLING_25HZ);
 
     // window
     window = window_create();
